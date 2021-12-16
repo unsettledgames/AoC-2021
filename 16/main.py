5a02c60c6771a66b6fdd60e3bff66428d7b206b4
Nicola <47360416+unsettledgames@users.noreply.github.com>
2021-12-16 20:10:21 +0100

    Added day 16

--- 16/main.py
+++ 16/main.py
@@ -1,76 +1,96 @@
+import math
+
+
 class Packet:
     def __init__(self, ver, pack_type, sub_packets):
         self.ver = ver
         self.pack_type = pack_type
         self.sub_packets = sub_packets
+        self.val = -1
 
+class PacketParser:
+    def __init__(self, packet):
+        self.packet = packet
+        self.curr = 0
+        self.start = 0
+        self.pack_len = len(packet)
+        self.length_type = 0
 
-def parse_packet(pack):
-    pack_len = 6
-    # Get version
-    ver = pack[0:3]
-    # Get type
-    pack_type = pack[3:6]
-    type_dec = int(pack_type, 2)
-    sub_packs = []
+    def read_ver(self):
+        ret = self.packet[self.curr : self.curr+3]
+        self.curr += 3
+        return ret
 
-    if type_dec != 4:
-        if pack[6] == "0":
-            pack_len += 16
-            tot_len = int(pack[7:22], 2)
-            # add packet version and length
-            prev_idx = 22
-            curr_idx = 25
+    def read_type(self):
+        ret = self.packet[self.curr : self.curr + 3]
+        self.curr += 3
+        return ret
 
-            pack_len += 3
-            # Get subpackets
-            while tot_len > 0:
-                if int(pack[prev_idx:prev_idx+3], 2) == 4:
-                    if pack[curr_idx] == '0':
-                        curr_idx += 5
-                        pack_len += 5
-                        sub_packs.append(parse_packet(pack[prev_idx:curr_idx]))
+    def read_length_type(self):
+        ret = self.packet[self.curr]
+        self.curr += 1
+        return ret
 
-                        tot_len -= (curr_idx - prev_idx)
-                        prev_idx = curr_idx
-                        curr_idx += 7
-                        pack_len += 7
-                    else:
-                        curr_idx += 5
-                        pack_len += 5
-                else:
-                    ret = parse_packet(pack[prev_idx:len(pack)])
-                    sub_packs.append(ret[1])
-                    tot_len -= ret[0]
-                    pack_len += ret[0]
+    def read_length(self):
+        ret = self.packet[self.curr : self.curr + 15]
+        self.curr += 15
+        return ret
+
+    def read_n_sub(self):
+        ret = self.packet[self.curr : self.curr + 11]
+        self.curr += 11
+        return ret
+
+    def read_next_packet(self):
+        ret = Packet("", "", [])
+
+        ret.ver = self.read_ver()
+        ret.pack_type = self.read_type()
+
+        if ret.pack_type == "100":
+            literal = self.read_literal()
+            ret.val = literal[0]
+            return [self.curr, ret]
         else:
-            n_packets = int(pack[7:18], 2)
-            prev_idx = 18
-            curr_idx = 24
-            i = 0
+            l_type = self.read_length_type()
+            if l_type == "0":
+                length = int(self.read_length(), 2)
 
-            while i < n_packets:
-                if int(pack[prev_idx:prev_idx+3], 2) == 4:
-                    if n_packets == 1:
-                        sub_packs.append(parse_packet(pack[prev_idx:len(pack)]))
-                        i += 1
-                    elif pack[curr_idx] == '0':
-                        curr_idx += 5
-                        sub_packs.append(parse_packet(pack[prev_idx:curr_idx]))
-                        prev_idx = curr_idx
-                        curr_idx += 6
+                parser = PacketParser(self.packet[self.curr:])
+                start_length = length
+                while length > 0:
+                    next_pack = parser.read_next_packet()
 
-                        i += 1
-                    else:
-                        curr_idx += 5
-                else:
-                    ret = parse_packet(pack[prev_idx:len(pack)])
-                    sub_packs.append(ret[1])
-                    i += 1
-    else:
+                    ret.sub_packets.append(next_pack[1])
+                    length = start_length - next_pack[0]
+                self.curr += next_pack[0]
+            else:
+                n_packets = int(self.read_n_sub(), 2)
+                for i in range(0, n_packets):
+                    parser = PacketParser(self.packet[self.curr:])
+                    next_pack = parser.read_next_packet()
+                    self.curr += next_pack[0]
+                    ret.sub_packets.append(next_pack[1])
+        return [self.curr, ret]
 
+    def read_literal(self):
+        end = self.curr
 
-    return Packet(ver, pack_type, sub_packs)
+        while self.packet[end] != "0":
+            end += 5
+
+        end += 5
+        ret = self.packet[self.curr: end]
+        length = end - self.curr
+        self.curr = end
+
+        ret_copy = ret
+        for i in range(0, len(ret)):
+            if i % 5 == 0:
+                ret_copy = ret_copy[0:i - int(i/5)] + ret_copy[i + 1 - int(i/5):]
+
+        return [ret_copy, length]
+
 
 def sum_ver(pack):
     my_sum = int(pack.ver, 2)
@@ -78,13 +98,52 @@ def sum_ver(pack):
         my_sum += sum_ver(i)
     return my_sum
 
+
 def part1(bins):
     my_sum = 0
 
     for bs in bins:
-        my_sum += sum_ver(parse_packet(bs))
+        parser = PacketParser(bs)
+        my_sum += sum_ver(parser.read_next_packet()[1])
 
     print(my_sum)
+
+
+def part2(bins):
+    for bs in bins:
+        parser = PacketParser(bs)
+        print(compute(parser.read_next_packet()[1]))
+
+
+def compute(packet):
+    if packet.pack_type == "000":
+        sum = 0
+        for i in packet.sub_packets:
+            sum += compute(i)
+        return sum
+    elif packet.pack_type == "001":
+        prod = 1
+        for i in packet.sub_packets:
+            prod *= compute(i)
+        return prod
+    elif packet.pack_type == "010":
+        ret = math.inf
+        for i in packet.sub_packets:
+            ret = min(ret, compute(i))
+        return ret
+    elif packet.pack_type == "011":
+        ret = -1
+        for i in packet.sub_packets:
+            ret = max(ret, compute(i))
+        return ret
+    elif packet.pack_type == "100":
+        return int(packet.val, 2)
+    elif packet.pack_type == "101":
+        return compute(packet.sub_packets[0]) > compute(packet.sub_packets[1])
+    elif packet.pack_type == "110":
+        return compute(packet.sub_packets[0]) < compute(packet.sub_packets[1])
+    elif packet.pack_type == "111":
+        return compute(packet.sub_packets[0]) == compute(packet.sub_packets[1])
 
 
 lines = open("input.txt", "r").readlines()
@@ -94,3 +153,4 @@ for line in lines:
     binaries.append(str(bin(int(line.rstrip(), 16))[2:].zfill(len(4 * line.rstrip()))))
 
 part1(binaries)
+part2(binaries)
